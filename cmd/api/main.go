9a45beac644c46701214cdc2ffc3a9a8fc1ca73e
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-03-14 20:14:43 -0500

    Load config from env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,31 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("PORT environment variable was nos found")
+	}
+
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	if DATABASE_URL == "" {
+		log.Fatal("DATABASE_URL environment variable was nos found")
+	}
+
 	config := Config{
-		Port:        "8080",
-		DatabaseURL: "postgres://postgres-url",
+		Port:        PORT,
+		DatabaseURL: DATABASE_URL,
 	}
 	s, err := NewServer(config)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Edigiraldo/car-rent
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+)
