81bf06e4df5dc3bf2090e17598be12d01d05439d
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-03-20 11:38:37 -0500

    Delete unnused method

--- internal/infrastructure/driven_adapters/repositories/postgress/postgress.go
+++ internal/infrastructure/driven_adapters/repositories/postgress/postgress.go
@@ -1,14 +1,10 @@
 package postgres
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 
 	_ "github.com/lib/pq"
-
-	"github.com/Edigiraldo/car-rent/internal/core/domain"
-	"github.com/Edigiraldo/car-rent/internal/infrastructure/driven_adapters/repositories/models"
 )
 
 type PostgresDB struct {
@@ -33,12 +29,4 @@ func (p *PostgresDB) GetDBHandle() *sql.DB {
 
 func (p *PostgresDB) Close() error {
 	return p.db.Close()
-}
-
-func (repo *PostgresDB) InsertCar(ctx context.Context, dc domain.Car) (err error) {
-	car := models.LoadCarFromDomain(dc)
-	_, err = repo.db.ExecContext(ctx, "INSERT INTO cars (id, type, seats, hourly_rent_cost, city, status) VALUES ($1, $2, $3, $4, $5, $6)",
-		car.ID, car.Type, car.Seats, car.HourlyRentCost, car.City, car.Status)
-
-	return err
 }
