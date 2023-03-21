eba6b52c93f87101688fcd3a3aaaa1fdc2432f71
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-03-21 13:33:15 -0500

    Include tests for cars handler

--- internal/infrastructure/driver_adapters/handlers/cars_test.go
+++ internal/infrastructure/driver_adapters/handlers/cars_test.go
@@ -96,8 +96,10 @@ func TestCarsRegister(t *testing.T) {
 			d := NewCarsDependencies(carsSrv)
 			test.setMocks(d)
 
+			baseURL := "/api/v1/"
 			body, _ := json.Marshal(test.args.car)
-			req, err := http.NewRequest("POST", "/cars", bytes.NewBuffer(body))
+			URL := baseURL + "cars/"
+			req, err := http.NewRequest(http.MethodGet, URL, bytes.NewBuffer(body))
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -194,8 +196,9 @@ func TestCarsGet(t *testing.T) {
 			values := url.Values{}
 			values.Set("id", test.args.requestID)
 			urlObj, _ := url.Parse(baseURL + "cars/" + values.Encode())
+			URL := urlObj.String()
 
-			req, err := http.NewRequest("GET", urlObj.String(), nil)
+			req, err := http.NewRequest(http.MethodGet, URL, nil)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -210,6 +213,130 @@ func TestCarsGet(t *testing.T) {
 
 			carsHandler := NewCars(carsSrv)
 			carsHandler.Get(rr, req)
+
+			assert.Equal(t, test.wants.statusCode, rr.Code)
+		})
+	}
+}
+
+func TestCarsFullUpdate(t *testing.T) {
+	car := dtos.Car{
+		ID:             uuid.New(),
+		Type:           "Sedan",
+		Seats:          4,
+		HourlyRentCost: 21.1,
+		City:           "Los Angeles",
+		Status:         "Available",
+	}
+
+	type args struct {
+		requestID string
+		car       dtos.Car
+	}
+	type wants struct {
+		statusCode int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wants    wants
+		setMocks func(*carsDependencies)
+	}{
+		{
+			name: "returns status code 200 when car was successfully updated",
+			args: args{
+				requestID: car.ID.String(),
+				car:       car,
+			},
+			wants: wants{
+				statusCode: http.StatusOK,
+			},
+			setMocks: func(d *carsDependencies) {
+				d.carsService.EXPECT().FullUpdate(gomock.Any(), car.ToDomain()).Return(nil)
+			},
+		},
+		{
+			name: "returns 400 status code when path param id is not an uuid",
+			args: args{
+				requestID: "this-is-an-not-uuid",
+			},
+			wants: wants{
+				statusCode: http.StatusBadRequest,
+			},
+			setMocks: func(d *carsDependencies) {
+			},
+		},
+		{
+			name: "returns 400 status code when car status in body is not allowed",
+			args: args{
+				requestID: car.ID.String(),
+				car: dtos.Car{
+					Status: "available",
+				},
+			},
+			wants: wants{
+				statusCode: http.StatusBadRequest,
+			},
+			setMocks: func(d *carsDependencies) {
+			},
+		},
+		{
+			name: "returns 404 status code when car was not found by id",
+			args: args{
+				requestID: car.ID.String(),
+				car:       car,
+			},
+			wants: wants{
+				statusCode: http.StatusNotFound,
+			},
+			setMocks: func(d *carsDependencies) {
+				d.carsService.EXPECT().FullUpdate(gomock.Any(), car.ToDomain()).Return(errors.New(services.ErrCarNotFound))
+			},
+		},
+		{
+			name: "returns 500 status code when car service fails to update the car",
+			args: args{
+				requestID: car.ID.String(),
+				car:       car,
+			},
+			wants: wants{
+				statusCode: http.StatusInternalServerError,
+			},
+			setMocks: func(d *carsDependencies) {
+				d.carsService.EXPECT().FullUpdate(gomock.Any(), car.ToDomain()).Return(errors.New("error registering car"))
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockCtlr := gomock.NewController(t)
+			carsSrv := mocks.NewMockCarsService(mockCtlr)
+			d := NewCarsDependencies(carsSrv)
+			test.setMocks(d)
+
+			baseURL := "/api/v1/"
+			values := url.Values{}
+			values.Set("id", test.args.requestID)
+			urlObj, _ := url.Parse(baseURL + "cars/" + values.Encode())
+			URL := urlObj.String()
+
+			body, _ := json.Marshal(test.args.car)
+			req, err := http.NewRequest(http.MethodPut, URL, bytes.NewBuffer(body))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Include request vars for gorilla mux to interpret path params
+			vars := map[string]string{
+				"id": test.args.requestID,
+			}
+			req = mux.SetURLVars(req, vars)
+
+			rr := httptest.NewRecorder()
+
+			carsHandler := NewCars(carsSrv)
+			carsHandler.FullUpdate(rr, req)
 
 			assert.Equal(t, test.wants.statusCode, rr.Code)
 		})
