f413b1962ff3591232780ac1ec34286525d3b098
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-04-11 20:33:39 -0500

    Include users db creation query

---
+++ db/migrations/3-users.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+CREATE TYPE USER_TYPES AS ENUM('Customer', 'Admin');
+CREATE TYPE USER_STATUSES AS ENUM('Active', 'Inactive');
+CREATE TABLE users (
+    id uuid NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(235) NOT NULL,
+    type USER_TYPES NOT NULL,
+    status USER_STATUSES NOT NULL
+);
