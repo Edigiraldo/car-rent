5abe5426893296066de96411335458cc1309e362
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-04-12 20:35:12 -0500

    Include test for Get in users handler

--- internal/infrastructure/driver_adapters/handlers/users_test.go
+++ internal/infrastructure/driver_adapters/handlers/users_test.go
@@ -6,12 +6,16 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/Edigiraldo/car-rent/internal/core/domain"
+	"github.com/Edigiraldo/car-rent/internal/core/services"
 	"github.com/Edigiraldo/car-rent/internal/infrastructure/driver_adapters/dtos"
 	mocks "github.com/Edigiraldo/car-rent/internal/pkg/mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -110,6 +114,109 @@ func TestUsersSingUp(t *testing.T) {
 
 			usersHandler := NewUsers(usersSrv)
 			usersHandler.SignUp(rr, req)
+
+			assert.Equal(t, test.wants.statusCode, rr.Code)
+		})
+	}
+}
+
+func TestUsersGet(t *testing.T) {
+	user := dtos.User{
+		ID:        uuid.New(),
+		FirstName: "Richard",
+		LastName:  "Feynman",
+		Email:     "richard.feynman@caltech.edu.us",
+		Type:      "Customer",
+		Status:    "Active",
+	}
+
+	type args struct {
+		requestID string
+	}
+	type wants struct {
+		statusCode int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wants    wants
+		setMocks func(*usersDependencies)
+	}{
+		{
+			name: "returns status code 200 when id is properly set and user was found",
+			args: args{
+				requestID: user.ID.String(),
+			},
+			wants: wants{
+				statusCode: http.StatusOK,
+			},
+			setMocks: func(d *usersDependencies) {
+				d.usersService.EXPECT().Get(gomock.Any(), user.ID).Return(user.ToDomain(), nil)
+			},
+		},
+		{
+			name: "returns 400 status code when path param id is not an uuid",
+			args: args{
+				requestID: "this-is-an-not-uuid",
+			},
+			wants: wants{
+				statusCode: http.StatusBadRequest,
+			},
+			setMocks: func(d *usersDependencies) {
+			},
+		},
+		{
+			name: "returns 404 status code when the user was not found",
+			args: args{
+				requestID: user.ID.String(),
+			},
+			wants: wants{
+				statusCode: http.StatusNotFound,
+			},
+			setMocks: func(d *usersDependencies) {
+				d.usersService.EXPECT().Get(gomock.Any(), user.ID).Return(domain.User{}, errors.New(services.ErrUserNotFound))
+			},
+		},
+		{
+			name: "returns 500 status code when there is a server error",
+			args: args{
+				requestID: user.ID.String(),
+			},
+			wants: wants{
+				statusCode: http.StatusInternalServerError,
+			},
+			setMocks: func(d *usersDependencies) {
+				d.usersService.EXPECT().Get(gomock.Any(), user.ID).Return(domain.User{}, errors.New("error getting user"))
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockCtlr := gomock.NewController(t)
+			usersSrv := mocks.NewMockUsersService(mockCtlr)
+			d := NewUsersDependencies(usersSrv)
+			test.setMocks(d)
+
+			baseURL := "/api/v1/"
+			urlObj, _ := url.Parse(baseURL + "users/" + test.args.requestID)
+			URL := urlObj.String()
+
+			req, err := http.NewRequest(http.MethodGet, URL, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Include request vars for gorilla mux to interpret path params
+			vars := map[string]string{
+				"id": test.args.requestID,
+			}
+			req = mux.SetURLVars(req, vars)
+
+			rr := httptest.NewRecorder()
+
+			usersHandler := NewUsers(usersSrv)
+			usersHandler.Get(rr, req)
 
 			assert.Equal(t, test.wants.statusCode, rr.Code)
 		})
