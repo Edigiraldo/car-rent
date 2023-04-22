6390c7a22298b8f86354796e37cd95287b345f44
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-04-22 12:08:31 -0500

    Change reservation table struct

--- db/migrations/m-4-reservations.sql
+++ db/migrations/m-4-reservations.sql
@@ -1,8 +1,14 @@
 DROP TABLE IF EXISTS reservations;
-CREATE TYPE RESERVATION_STATUSES AS ENUM('Active', 'Cancelled');
+CREATE TYPE RESERVATION_STATUSES AS ENUM('Reserved', 'Canceled', 'Completed');
+CREATE TYPE PAYMENT_STATUSES AS ENUM('Paid', 'Pending', 'Canceled');
 CREATE TABLE reservations (
     id uuid PRIMARY KEY NOT NULL,
-    user_id uuid NOT NULL,
+    user_id uuid NOT NULL REFERENCES users(id),
     car_id uuid NOT NULL REFERENCES cars(id),
-    status RESERVATION_STATUSES NOT NULL DEFAULT 'Active'
+    status RESERVATION_STATUSES NOT NULL DEFAULT 'Reserved',
+    payment_status PAYMENT_STATUSES NOT NULL DEFAULT 'Pending',
+    start_date TIMESTAMPTZ,
+    end_date TIMESTAMPTZ
 );
+CREATE INDEX reservations_user_id_idx ON reservations (user_id);
+CREATE INDEX reservations_car_id_idx ON reservations (car_id);
