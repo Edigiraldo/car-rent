29cb23284c7a91b9b426a68a76ac9c7b55bf86d5
Edison Giraldo A <edison.giraldoa12@gmail.com>
2023-05-17 20:01:37 -0500

    Make reservations to be deleted on cascade

--- db/migrations/m-4-reservations.sql
+++ db/migrations/m-4-reservations.sql
@@ -3,8 +3,8 @@ CREATE TYPE RESERVATION_STATUSES AS ENUM('Reserved', 'Canceled', 'Completed');
 CREATE TYPE PAYMENT_STATUSES AS ENUM('Paid', 'Pending', 'Canceled');
 CREATE TABLE reservations (
     id uuid PRIMARY KEY NOT NULL,
-    user_id uuid NOT NULL REFERENCES users(id),
-    car_id uuid NOT NULL REFERENCES cars(id),
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    car_id uuid NOT NULL REFERENCES cars(id) ON DELETE CASCADE,
     status RESERVATION_STATUSES NOT NULL DEFAULT 'Reserved',
     payment_status PAYMENT_STATUSES NOT NULL DEFAULT 'Pending',
     start_date TIMESTAMPTZ,
